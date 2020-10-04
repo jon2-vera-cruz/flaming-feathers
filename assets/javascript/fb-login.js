09c67e656c6d6520e1b12e636b51ea4e8717de92
jon2-vera-cruz <jon2_vera_cruz@hotmail.com>
2020-10-04 17:29:33 +0000

    ms2-project-version 1.0.3 - Created App on Facebook for Developer

--- assets/javascript/fb-login.js
+++ assets/javascript/fb-login.js
@@ -1,29 +1,57 @@
+/* https://stackoverflow.com/questions/36383243/javascript-login-with-facebook-display-users-profile-picture-after-logged-in */
+
 window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '{your-app-id}',
-        cookie     : true,
-        xfbml      : true,
-        version    : '{api-version}'
-    });
-      
-    FB.AppEvents.logPageView();   
-      
-};
+  FB.init({
+    appId: '1034762986580727',
+    xfbml: true,
+    version: 'v2.5'
+  });
 
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
-
-function checkLoginState() {
   FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
+    if (response.status === 'connected') {
+      document.getElementById('status').innerHTML = 'We are connected.';
+      document.getElementById('login').style.visibility = 'hidden';
+    } else if (response.status === 'not_authorized') {
+      document.getElementById('status').innerHTML = 'We are not logged in.'
+    } else {
+      document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+    }
+  });
+};
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// login with facebook with extra permissions
+function fblogin() {
+  FB.login(function(response) {
+    if (response.status === 'connected') {
+      document.getElementById('status').innerHTML = 'We are connected.';
+      document.getElementById('login').style.visibility = 'hidden';
+      getInfo(); // Invoke it here
+    } else if (response.status === 'not_authorized') {
+      document.getElementById('status').innerHTML = 'We are not logged in.'
+    } else {
+      document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+    }
+  }, {
+    scope: 'email'
+  });
+}
+
+// getting basic user info
+function getInfo() {
+  FB.api('/me', 'GET', {
+    fields: 'first_name,last_name,name,id,picture.width(150).height(150)'
+  }, function(response) {
+    console.log(response);//Check the response in console
+    document.getElementById('status').innerHTML = "<img src='" + response.picture.data.url + "'>";
   });
 }
